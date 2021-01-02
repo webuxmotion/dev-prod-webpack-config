4973860cbd71912c64d09f7c96f255213099d0ce
Andrii Pereverziev <andriipereverziev@Andriis-MacBook-Pro.local>
2021-01-02 13:31:44 +0200

    Use html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const path = require("path");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
     entry: "./src/index.js",
     output: {
-        filename: "main.js",
+        filename: "main-[contenthash].js",
         path: path.resolve(__dirname, "dist")
     },
     module: {
@@ -19,4 +20,9 @@ module.exports = {
             },
         ],
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/template.html"
+        })
+    ]
 };

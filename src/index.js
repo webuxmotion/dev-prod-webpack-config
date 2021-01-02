5ae9d0d3811fbb2123d7155a672f4c6282ee9d68
Andrii Pereverziev <andriipereverziev@Andriis-MacBook-Pro.local>
2021-01-02 13:19:55 +0200

    style-loader, css-loader, scss-loader

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import { run } from './app/app';
+import './main.scss';
+import './main.css';
 import { AlertService } from './app/alert.service';
 import { ComponentService } from './app/component.service';
 
 const alertService = new AlertService();
 const componentService = new ComponentService();
-
+ 
 run(alertService, componentService);
--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,17 @@ module.exports = {
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist")
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.scss$/i,
+                use: ["style-loader", "css-loader", "sass-loader"],
+            },
+        ],
+    },
 };
